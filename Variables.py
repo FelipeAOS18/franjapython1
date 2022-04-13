0d5f89e9bc8fd1b38adcb7fe3bbdc733bca3c4e0
FelipeAOS18 <felipeolguin@alumnos.upla.cl>
2022-04-13 17:15:59 -0400

    Operadores aritmeticos y variables
    Clase 13-04

---
+++ Variables.py
@@ -0,0 +1,14 @@
+numero=3
+print(numero)
+numero1=5
+numero2=3
+print(numero-numero1*numero2)
+numero= numero1**numero2
+print(numero)
+#1.las variables no pueden tener espacio
+#2. las variables no pueden tener signos especiales ej $%$&$
+#3. Siempre las variables en minuscula 
+#4. Si UNA VARIABLE TIENE MAS DE UNA PALABRA LOS DEBO SEPARA POR UN "_"
+#5 No pueden ser un numero
+#Reglas de procedimiento
+print(5+(5*2))
