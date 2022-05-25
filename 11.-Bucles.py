5048bdaf70f079b64aadd1ecc875e0f5c6d6184a
FelipeAOS18 <felipeolguin@alumnos.upla.cl>
2022-05-25 15:36:00 -0400

    clase 25-05-2022

---
+++ 11.-Bucles.py
@@ -0,0 +1,23 @@
+#bucles
+# from traceback import print_tb
+# contador=0
+# print('2 eleveado a '+ str(contador)+ ' es igual a:'+ str(2**contador))
+# contador=1
+# print('2 eleveado a '+ str(contador)+ ' es igual a:'+ str(2**contador))
+# contador=2
+# print('2 eleveado a '+ str(contador)+ ' es igual a:'+ str(2**contador))
+#bucle
+# contador=10
+# while contador > 0:
+#     print('2 eleveado a '+ str(contador)+ ' es igual a:'+ str(2**contador))
+#     contador-=1
+#bucle for
+# contador =1
+# while  contador <100:
+#     print (contador)
+#     contador +=1
+# tipo de dato rango
+rango=range(100)
+print(rango)
+for contador in range(1,100):
+    print(contador**2)
---
+++ 12.-Recorrer string.py
@@ -0,0 +1,7 @@
+#Recorrer string
+def main():
+    nombre=input("ingrese su nombre: ")
+    for letra in nombre :
+        print(letra)
+if __name__ == '__main__':
+    main()
